239492abe0c2a56287398d8dcf4ccc4d046b423f
alumno <alumno@dam206>
2024-10-10 12:43:36 +0200

    Para hacer

---
+++ Tarea4Johann/src/tarea4/Alumnos.java
@@ -0,0 +1,5 @@
+package tarea4;
+
+public class Alumnos {
+
+}
---
+++ Tarea4Johann/src/tarea4/AlumnosMain.java
@@ -0,0 +1,10 @@
+package tarea4;
+
+public class AlumnosMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
